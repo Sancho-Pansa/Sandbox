e7b3a0ba4d2c19d5be2b4927baaea681d1f43329
Zelgadis <zelgadis4@gmail.com>
2017-05-12 20:33:56 +0300

    added comments

--- CircularLinkedList/src/main/CircularLinkedList.java
+++ CircularLinkedList/src/main/CircularLinkedList.java
@@ -3,6 +3,17 @@ package main;
 import java.util.*;
 import java.lang.StackOverflowError;
 
+/**
+ * This class is used to create circular linked list.
+ * This list acts like usual linked list
+ * However the last item points on the first element and vice versa
+ * 
+ * @author SanchoPansa
+ * 
+ * @param <T>
+ * @version 1.0
+ */
+
 public class CircularLinkedList<T>
 {
 	private StackOverflowError overflowException = new StackOverflowError("Index overflow");
@@ -20,6 +31,10 @@ public class CircularLinkedList<T>
 		
 	}
 	
+	/**
+	 * Pushes next element into the list 
+	 * @param t
+	 */
 	public void push(T t)
 	{
 		if(isClear)
@@ -43,6 +58,10 @@ public class CircularLinkedList<T>
 		size++;
 	}
 	
+	/**
+	 * Inserts element as first element of list
+	 * @param t
+	 */
 	public void addFirst(T t)
 	{
 		Node<T> addingNode = new Node<>(t);
@@ -57,6 +76,11 @@ public class CircularLinkedList<T>
 		size++;
 	}
 	
+	/**
+	 * Inserts element on specified index
+	 * @param t
+	 * @param index
+	 */
 	public void add(T t, int index)
 	{
 		if(index > size)
@@ -83,6 +107,10 @@ public class CircularLinkedList<T>
 		size++;
 	}
 	
+	/**
+	 * Inserts element on specified index
+	 * @return
+	 */
 	public T pop()
 	{
 		T t = lastNode.getContent();
@@ -94,11 +122,20 @@ public class CircularLinkedList<T>
 		return t;
 	}
 	
+	/**
+	 * Returns last element of circular list, but do not removes it as pop()
+	 * @return
+	 * @throws NoSuchElementException
+	 */
 	public T getLast() throws NoSuchElementException
 	{
 		return lastNode.getContent();
 	}
 
+	/**
+	 * Returns size of list
+	 * @return
+	 */
 	public int size() 
 	{
 		return size;
